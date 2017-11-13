062fc3da1b6ac94f8394b599e79627044c80acd1
Kirk Hess <kirk.hess@gmail.com>
2017-11-13 12:56:40 +0000

    Revert "new pilot data"
    This reverts commit 1ed39836daa4890e5a559466b2570d382efbdbfa.

--- reload.js
+++
@@ -1,9 +0,0 @@
-var fs = require("fs");
-var request = require("request");
-var content = fs.readFileSync("bfdump.json");
-var bfdump = JSON.parse(content)
-for (var i in bfdump){
-    console.log(i);
-    delete bfdump[i].id
-    request.post({url:'http://bibframe.org/bibliomata/verso/api/bfs', header:'Content-Type: application/json', json:bfdump[i]}, function(err, res, body){console.log("error");})
-}
