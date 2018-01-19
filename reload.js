dde01e5082275eae99c49054ca099d64d64832a6
Kirk Hess <kirk.hess@gmail.com>
2018-01-19 09:32:35 -0500

    pilot data for posting

---
+++ reload.js
@@ -0,0 +1,12 @@
+
+var fs = require("fs");
+var request = require("request");
+
+var content = fs.readFileSync("bfdump.json");
+
+var bfdump = JSON.parse(content)
+
+for (var i in bfdump){ 
+ delete bfdump[i].id 
+ request.post({url:'http://mlvlp04.loc.gov:3000/verso/api/bfs', header:'Content-Type: application/json', json:bfdump[i]}, function(err, res, body){console.log(err);})
+}
