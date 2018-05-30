3618b613802d21bb65ff2cd2a4c05d085c284309
Wayne Schneider <wayne@indexdata.com>
2018-05-30 18:22:58 -0500

    Update "metadata" property of Config model with before-save hook.
    Towards #1.

--- common/models/config.js
+++ common/models/config.js
@@ -1,5 +1,40 @@
 'use strict';
 
 module.exports = function(Config) {
-
+  Config.observe('before save', function updateMetadata(ctx, next) {
+    // manage metadata property
+    const d = new Date();
+    const token = ctx.options && ctx.options.accessToken;
+    const userId = token && token.userId;
+    if (ctx.instance) {
+      if (ctx.isNewInstance === true) {
+        ctx.instance.metadata = {
+          createDate: d,
+          updateDate: d,
+          createUser: userId,
+          updateUser: userId
+        };
+        next();
+      } else {
+        Config.findById(ctx.instance.id,{ fields: { metadata: true } }, function(err, instance) {
+          if (instance.metadata == null) {
+            instance.metadata = {};
+          }
+          instance.metadata.updateDate = d;
+          instance.metadata.updateUser = userId;
+          ctx.instance.metadata = instance.metadata;
+          next();
+        });
+      }
+    } else {
+      let metadata = ctx.currentInstance.metadata;
+      if (metadata == null) {
+        metadata = {}
+      }
+      metadata.updateDate = d;
+      metadata.updateUser = userId;
+      ctx.data.metadata = metadata;
+      next();
+    }
+  });
 };
