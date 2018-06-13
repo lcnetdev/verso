81bd7b0c3b2172749f57859f735a2dc53fb88f0f
Charles Ledvina <charles@indexdata.com>
2018-06-13 17:23:34 -0500

    Added new script to load vocabs on server boot.

---
+++ server/boot/load-vocabs.js
@@ -0,0 +1,45 @@
+// Seed Vocabularies into storage if they aren't already there
+// Read all .rdf files from data/vocabularies
+// Use the file name as the vocabulary name
+'use strict';
+
+module.exports = function(app, cb) {
+  /*
+   * The `app` object provides access to a variety of LoopBack resources such as
+   * models (e.g. `app.models.YourModelName`) or data sources (e.g.
+   * `app.datasources.YourDataSource`). See
+   * http://docs.strongloop.com/display/public/LB/Working+with+LoopBack+objects
+   * for more info.
+   */
+  const vocabPath = 'data/vocabularies';
+  const Config = app.models.Config;
+  Config.count({configType: 'vocabulary'}, function(err, count) {
+    const fs = require('fs');
+    if (err) { return console.warn(err.message); }
+    if (count) {
+      console.log('Skipping vocabulary load (datastore is populated)');
+    } else {
+      const vocabFiles = fs.readdirSync(vocabPath);
+      let data = [];
+      for (let i = 0; i < vocabFiles.length; i++) {
+        let path = vocabPath + '/' + vocabFiles[i];
+        if ((vocabFiles[i].search('\.rdf$') != -1) &&
+            (fs.statSync(path).isDirectory() === false)) {
+          const name = vocabFiles[i].substr(0, vocabFiles[i].length - 3);
+          const json = fs.readFileSync(path, {encoding: 'utf8'});
+          data.push({
+            name: name,
+            configType: 'vocabulary',
+            json: json,
+          });
+        }
+      }
+      Config.create(data, function(err, models) {
+        if (err) { return console.warn(err); }
+        console.log('Created ' + models.length + ' vocabularies');
+      });
+    }
+  });
+
+  process.nextTick(cb); // Remove if you pass `cb` to an async function yourself
+};
