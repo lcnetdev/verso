d0500bcc053f3e1dce2dbc308632771216243e41
Kirk Hess <kirk.hess@gmail.com>
2018-06-26 12:35:01 +0000

    fix profiles; add dump file

--- reload.js
+++ reload.js
@@ -2,11 +2,25 @@
 var fs = require('fs');
 var request = require('request');
 
-var content = fs.readFileSync('data/bfdump.json');
+var content = fs.readFileSync('data/bfdump_eval.json');
 
 var bfdump = JSON.parse(content);
 
 for (var i in bfdump) {
   delete bfdump[i].id;
-  request.post({url: 'http://mlvlp04.loc.gov:3000/verso/api/bfs', header: 'Content-Type: application/json', json: bfdump[i]}, function(err, res, body) { console.log(err); });
+  request.post({url: 'http://localhost:3001/api/bfs/', 
+                header: 'Content-Type: application/json', 
+                json: bfdump[i]},
+                function(err, res, body) { 
+                if (err){
+                    console.log(err);
+                } else {
+                    console.log(body.name);
+                } 
+                });
+//                ).on('error', (err) => {
+//                    console.error(err);
+//                }).on('finish'), (res) => {
+//                    console.log(i)
+//                };
 }
