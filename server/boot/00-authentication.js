1a2f73e88489676350f3924e5dffc2cc22ee4041
Wayne Schneider <wayne@indexdata.com>
2018-09-14 11:16:57 -0500

    Add access_id cookie on login for other applications to access.
    Stores identity information in JSON. For now, just:
    ```
    {
      "username": "myUser",
      "roles": [
        "roleName"
      ]
    }
    ```

--- server/boot/00-authentication.js
+++ server/boot/00-authentication.js
@@ -6,6 +6,19 @@ module.exports = function enableAuthentication(server) {
 
   // Respond to login with token in cookie
   server.models.User.afterRemote('login', function(ctx) {
+    server.models.User.findById(
+      ctx.result.userId,
+      {include: 'roles'},
+      function(err, instance) {
+        if (err) throw err;
+        const user = instance.toJSON();
+        var roles = [];
+        user.roles.forEach(function(role) {
+          roles.push(role.name);
+        });
+        const userId = {username: user.username, roles: roles};
+        ctx.res.cookie('access_id', JSON.stringify(userId));
+      });
     ctx.res.cookie('access_token', ctx.result.id,
       {signed: true, expires: 0, httpOnly: true});
     return Promise.resolve();
