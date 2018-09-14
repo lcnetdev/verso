923634b94465d471408fe6f0ca1a3e1b00343e3c
Jason Skomorowski <jason@indexdata.com>
2018-09-13 22:26:59 -0400

    Store auth token in an httpOnly session cookie

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -1,6 +1,19 @@
 'use strict';
 
 module.exports = function enableAuthentication(server) {
-  // enable authentication
+  // Enable authentication
   server.enableAuth();
+
+  // Respond to login with token in cookie
+  server.models.User.afterRemote('login', function(ctx) {
+    ctx.res.cookie('access_token', ctx.result.id,
+      {signed: true, expires: 0, httpOnly: true});
+    return Promise.resolve();
+  });
+
+  // Clear cookie on logout
+  server.models.User.afterRemote('logout', function(ctx) {
+    ctx.res.clearCookie('access_token');
+    return Promise.resolve();
+  });
 };
