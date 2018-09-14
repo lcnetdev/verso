c5e09de0ae31c6b9acab162ef55444ecfad2b7ba
Wayne Schneider <wayne@indexdata.com>
2018-09-14 11:31:27 -0500

    Rename `access_id` cookie to `current_user`. Add `id` property to `current_user` cookie.
    Also delete the cookie on logout.

--- server/boot/00-authentication.js
+++ server/boot/00-authentication.js
@@ -4,7 +4,7 @@ module.exports = function enableAuthentication(server) {
   // Enable authentication
   server.enableAuth();
 
-  // Respond to login with token in cookie
+  // Respond to login with user identity and token in cookies
   server.models.User.afterRemote('login', function(ctx) {
     server.models.User.findById(
       ctx.result.userId,
@@ -16,17 +16,22 @@ module.exports = function enableAuthentication(server) {
         user.roles.forEach(function(role) {
           roles.push(role.name);
         });
-        const userId = {username: user.username, roles: roles};
-        ctx.res.cookie('access_id', JSON.stringify(userId));
+        const currentUser = {
+          id: ctx.result.userId,
+          username: user.username,
+          roles: roles,
+        };
+        ctx.res.cookie('current_user', JSON.stringify(currentUser));
       });
     ctx.res.cookie('access_token', ctx.result.id,
       {signed: true, expires: 0, httpOnly: true});
     return Promise.resolve();
   });
 
-  // Clear cookie on logout
+  // Clear cookies on logout
   server.models.User.afterRemote('logout', function(ctx) {
     ctx.res.clearCookie('access_token');
+    ctx.res.clearCookie('current_user');
     return Promise.resolve();
   });
 };
