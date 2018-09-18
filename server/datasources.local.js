da264736dda9d94b88adc1c66b69ea9cba84575b
Kirk Hess <kirk.hess@gmail.com>
2018-09-18 14:54:03 +0000

    various fixes

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,6 +1,8 @@
 // configure storage based on environment variables
 'use strict';
 
+console.log(process.env.DB_STORAGE);
+console.log(process.env.DB_FILE);
 if (process.env.DB_STORAGE === 'file') {
   module.exports.db = {
     connector: 'memory',
