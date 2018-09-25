2a45c8c88b099c940a11f0df8d498a4b5def1680
Kirk Hess <kirk.hess@gmail.com>
2018-09-25 21:07:19 +0000

    lint fixes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,21 @@
-//sudo DB_STORAGE=mongodb DB_URL=mongodb://127.0.0.1:27017/bfpilot pm2 start -n verso ./server/server.js --update-env
+// sudo DB_STORAGE=mongodb DB_URL=mongodb://127.0.0.1:27017/bfpilot pm2 start -n verso ./server/server.js --update-env
 
 module.exports = {
-  apps : [
-      {
-        name: "verso",
-        script: "./server/server.js",
-        watch: false,
-        env: {
-            "AUTH": false,
-            "DB_STORAGE":"file",
-            "DB_FILE": "/opt/bibliomata/verso/bfpilot.json"
-        },
-        env_mongo: {
-            "AUTH": true,
-            "DB_URL":"mongodb://127.0.0.1:27017/bfpilot",
-            "DB_STORAGE":"mongodb"
-        }
-      }
-  ]
-}
+  apps: [
+    {
+      name: 'verso',
+      script: './server/server.js',
+      watch: false,
+      env: {
+        'AUTH': false,
+        'DB_STORAGE': 'file',
+        'DB_FILE': '/opt/bibliomata/verso/bfpilot.json',
+      },
+      env_mongo: {
+        'AUTH': true,
+        'DB_URL': 'mongodb://127.0.0.1:27017/bfpilot',
+        'DB_STORAGE': 'mongodb',
+      },
+    },
+  ],
+};
