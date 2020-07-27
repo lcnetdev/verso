8231340f0510a3080c3410d998981526962d90e0
Kevin Ford <kefo@loc.gov>
2020-07-27 14:52:06 -0400

    Trying to set up a staging environment.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,17 +12,18 @@ module.exports = {
         'DB_FILE': './bfpilot.json',
         'NODE_OPTIONS': '--max_old_space_size=4096'
       },
+      env_staging: {
+        'AUTH': false,
+        'DB_STORAGE': 'file',
+        'DB_FILE': './bfpilot_staging.json',
+        'NODE_OPTIONS': '--max_old_space_size=4096'
+      },
       env_mongo: {
         'AUTH': true,
         'DB_URL': 'mongodb://127.0.0.1:27017/bfpilot',
         'DB_STORAGE': 'mongodb',
       },
-    },
-    {
-      name: 'recto',
-      script: '../recto/server.js',
-      watch: false,
-    },
+    }
   ],
 };
 
